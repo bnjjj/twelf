d4f4f307c137bdc17858fc33063eb62b6de57405
Ch. König <cking@users.noreply.github.com>
2022-03-25 12:14:32 +0100

    make formats configurable (#13)
    * make formats configurable
    
    * handle errors as well
    
    * json is a required dep
    
    Signed-off-by: Kura <me@kurabloodlust.eu>
    
    * json isnt optional anymore
    
    Signed-off-by: Kura <me@kurabloodlust.eu>
    
    * exhaust list
    
    Signed-off-by: Kura <me@kurabloodlust.eu>
    
    * example of how to introduce features
    
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
    
    * default feature set fix
    
    Co-authored-by: Coenen Benjamin <benjamin.coenen@hotmail.com>
    
    * proper derive code gen
    
    * json error is always needed
    
    Signed-off-by: Kura <me@kurabloodlust.eu>
    
    Co-authored-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
    Co-authored-by: Coenen Benjamin <benjamin.coenen@hotmail.com>

--- twelf/src/error.rs
+++ twelf/src/error.rs
@@ -5,16 +5,21 @@ use thiserror::Error as ErrorTrait;
 pub enum Error {
     #[error("io error")]
     Io(#[from] std::io::Error),
+    #[cfg(feature = "env")]
     #[error("envy serde error")]
     Envy(#[from] envy::Error),
     #[error("json serde error")]
     Json(#[from] serde_json::Error),
+    #[cfg(feature = "toml")]
     #[error("toml serde error")]
     Toml(#[from] toml_rs::de::Error),
+    #[cfg(feature = "yaml")]
     #[error("yaml serde error")]
     Yaml(#[from] serde_yaml::Error),
+    #[cfg(feature = "ini")]
     #[error("ini serde error")]
     Ini(#[from] serde_ini::de::Error),
+    #[cfg(feature = "dhall")]
     #[error("dhall serde error")]
     Dhall(#[from] serde_dhall::Error),
     #[error("invalid format")]
