bd75452924eb9ffcc8abdce8b38c6cce5c3ab058
Luke Peterson <luke@infoplatter.com>
2023-06-21 15:15:00 +0900

    Adding CustomFn layer, where any Fn type, (functions or immutable closures) can provide a values.  This is useful to support additional data sources, for example a database or an unsupported file format, e.g. hjson

---
+++ twelf/tests/custom_fn.rs
@@ -0,0 +1,31 @@
+
+use config_derive::config;
+use twelf::Layer;
+
+use serde_json::value::to_value;
+
+#[test]
+fn simple_custom_fn() {
+    #[config]
+    #[derive(Debug, Default)]
+    struct TestCfg {
+        test: String,
+        another: usize,
+    }
+
+    std::env::set_var("ANOTHER", "5");
+
+    let func = || {
+        to_value(TestCfg{test: "from func".to_string(), another: 25}).unwrap()
+    };
+
+    let prio = vec![Layer::CustomFn(func.into()), Layer::Env(None)];
+    let config = TestCfg::with_layers(&prio).unwrap();
+    assert_eq!(config.test, String::from("from func"));
+    assert_eq!(config.another, 5usize);
+
+    let prio = vec![Layer::Env(None), Layer::CustomFn(func.into())];
+    let config = TestCfg::with_layers(&prio).unwrap();
+    assert_eq!(config.test, String::from("from func"));
+    assert_eq!(config.another, 25usize);
+}
